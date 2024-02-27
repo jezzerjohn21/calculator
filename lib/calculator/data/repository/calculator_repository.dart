20eb9c88ab6695c648ed73f8189a02c0db8372a7
Medjuil <mailuser0110@gmail.com>
2024-02-27 13:28:34 +0800

    adding test coverage. all passed 100%

--- lib/calculator/data/repository/calculator_repository.dart
+++ lib/calculator/data/repository/calculator_repository.dart
@@ -1,12 +1,4 @@
 class CalculatorRepository {
-  // late double _num1;
-  // late double _num2;
-
-  // CalculatorRepository() {
-  //   _num1 = 0;
-  //   _num2 = 0;
-  // }
-
   int addition(int num1, int num2) {
     return num1 + num2;
   }
@@ -16,7 +8,7 @@ class CalculatorRepository {
   }
 
   double division(double num1, double num2) {
-    return num1 == 0 || num2 == 0? throw Exception() : num1 / num2;
+    return num1 == 0 || num2 == 0 ? throw Exception() : num1 / num2;
   }
 
   double multplication(double num1, double num2) {
